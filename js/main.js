b2678596fa19e1f1aee2adb02cc32f7c30f382f5
Markinsky <carlosgerardo1957@hotmail.com>
2023-03-30 16:12:29 -0600

    Agregamos funcionalidad y validaciones

--- js/main.js
+++ js/main.js
@@ -7,6 +7,32 @@ let btnClear = document.getElementById("btnClear");
 
 let alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
 let alertValidaciones = document.getElementById("alertValidaciones");
+
+let isValid = true;
+let idTimeOut;
+let precio = 0;
+let contador = 0;
+let totalEnProductos = 0;
+let costoTotal = 0;
+
+let tabla = document.getElementById("tablaListaCompras");
+let cuerpoTabla = tabla.getElementsByTagName("tbody");
+
+let contadorProductos = document.getElementById("contadorProductos");
+let productosTotal = document.getElementById("productosTotal");
+let precioTotal = document.getElementById("precioTotal");
+
+function validarCantidad(){
+    if( (txtNumero.value.length==0) || (isNaN(txtNumero.value))||(parseFloat(txtNumero.value)<=0)){
+        return false;
+    }
+    return true;
+};
+
+function setPrecio(){
+    return Math.floor(Math.random() * 50 * 100) / 100;
+};
+
 btnAgregar.addEventListener("click", function(event){
     event.preventDefault();
 });
@@ -14,15 +40,30 @@ btnAgregar.addEventListener("click", function(event){
 btnClear.addEventListener("click", function(event){
     event.preventDefault();
     txtNombre.value="";
-    txtNumber.value="";
+    txtNumero.value="";
+    contadorProductos.innerText = "";
+
+    contador = 0;
+    totalEnProductos = 0;
+    costoTotal = 0;
+    productosTotal.innerText="0";
+    precioTotal.innerText = "$0";
+    contadorProductos.innerText = "0";
+
+    localStorage.setItem("contadorProductos", contador);
+    localStorage.setItem("totalEnProductos", totalEnProductos);
+    localStorage.setItem("costoTotal", costoTotal.toFixed(2));
+    
 });
 
 btnAgregar.addEventListener("click", function(event){
     event.preventDefault();
+    isValid = true;
+    clearTimeout(idTimeOut); 
     alertValidacionesTexto.innerHTML ="";
     alertValidaciones.style.display = "none";
     let lista = "Los siguientes campos deben estar llenados correctamente <ul>";
-    if(txtNombre.value.length==0){
+    if(txtNombre.value.length<2){
         txtNombre.style.border="solid thin red";
         //alertValidacionesTexto.innerHTML = "Se debe escribir un nombre ";
         lista += "<li> Se debe escribir un nombre valido </li>";
@@ -30,16 +71,41 @@ btnAgregar.addEventListener("click", function(event){
     }else{  
         txtNombre.style.border=""; 
     }
-    if(txtNumero.value.length==0){
+    if(! validarCantidad()){
         txtNumero.style.border="solid thin red";
        // alertValidacionesTexto.innerHTML += "Se debe escribir un numero";
        lista += "<li> Se debe escribir una cantidad valida </li>";
         alertValidaciones.style.display = "block";
+        isValid = false;
     }else{
         txtNumero.style.border=""; 
     }
+
     lista+="</ul>";
     alertValidacionesTexto.insertAdjacentHTML("beforeend", lista);
+    idTimeOut = setTimeout(function(){
+        alertValidaciones.style.display = "none";
+    }, 5000);
+    precio = setPrecio();
+    contador ++;
+    let row = `<tr> 
+                <td>${contador}</td>
+                <td>${txtNombre.value}</td>
+                <td>${txtNumero.value}</td>
+                <td>${precio}</td>
+             </tr>`;
+             cuerpoTabla[0].insertAdjacentHTML("beforeend", row);
+             contadorProductos.innerText = contador;
+             costoTotal += precio * parseFloat(txtNumero.value);
+             totalEnProductos += parseFloat(txtNumero.value);
+             productosTotal.innerText= totalEnProductos;
+             precioTotal.innerText = costoTotal;
+             localStorage.setItem("contadorProductos", contador);
+             localStorage.setItem("totalEnProductos", totalEnProductos);
+             localStorage.setItem("costoTotal", costoTotal.toFixed(2));
+             txtNombre.value="";
+             txtNumero.value="";
+             txtNombre.focus();
 });
 
 txtNombre.addEventListener("blur", function(event){
@@ -47,7 +113,7 @@ txtNombre.addEventListener("blur", function(event){
     txtNombre.value = txtNombre.value.trim();
 });
 
-txtNumber.addEventListener("blur", function(event){
+txtNumero.addEventListener("blur", function(event){
     event.preventDefault();
-    txtNumber.value = txtNumber.value.trim();
+    txtNumero.value = txtNumero.value.trim();
 });
